1e1da2df76e6a26166342207cddd257f8530d03d
Jaswal1p <Jaswal1p@yahoo.com>
2023-06-10 12:37:49 -0400

    added components and entered into app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
+
+
+
 
 function App() {
   return (
     <div className="App">
       
-      <h2>Working</h2>
+       <Navbar/>
+       <Home/>
+       <Main/>
+       <Footer/>
+      
     </div>
   );
 }
